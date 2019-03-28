d5a072612159eaf62cfc260c627adc572cae0b6d
Pedro de Vasconcellos Oporto <pdevasconcelloso@wpi.edu>
2019-03-28 13:41:26 -0400

    Update Event.java
    Modified indentation to 2sps in Event.java

--- src/main/java/com/google/codeu/data/Event.java
+++ src/main/java/com/google/codeu/data/Event.java
@@ -6,91 +6,91 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 enum PublicType{
-    ALLPUBLIC, CAMPUSWIDE, INVITEONLY;
+  ALLPUBLIC, CAMPUSWIDE, INVITEONLY;
 }
 
 public class Event {
     
-    private UUID eventId;
-    private String speaker;
-    private String organization;
-    private Date eventDate;
-    private Location location;
-    private List<String> amenities;
-    private String externalLink;
-    private PublicType publicType;
-    private int ownerId;
-    private List<ThreadComment> thread;
-    private long timeStamp;
-    
-    public Event(String speaker, String organization, Date eventDate, Location location, List<String> amenities, String externalLink, PublicType publicType, int ownerId){
-        this(UUID.randomUUID(), System.currentTimeMillis(), speaker, organization, eventDate, location, amenities, externalLink, publicType, ownerId);
-    }
-    
-    public Event(UUID id, long timeStamp, String speaker, String organization, Date eventDate, Location location, List<String> amenities, String externalLink, PublicType publicType, int ownerId){
-        this.eventId = id;
-        this.speaker = speaker;
-        this.organization = organization;
-        this.eventDate = eventDate;
-        this.location = location;
-        this.amenities = amenities;
-        this.externalLink = externalLink;
-        this.publicType = publicType;
-        this.ownerId = ownerId;
-        this.thread = new ArrayList<ThreadComment>();
-        this.timeStamp = timeStamp;
-    }
-    
-    public UUID getEventId(){
-        return eventId;
-    }
+  private UUID eventId;
+  private String speaker;
+  private String organization;
+  private Date eventDate;
+  private Location location;
+  private List<String> amenities;
+  private String externalLink;
+  private PublicType publicType;
+  private int ownerId;
+  private List<ThreadComment> thread;
+  private long timeStamp;
 
-    public String getSpeaker(){
-        return speaker;
-    }
+  public Event(String speaker, String organization, Date eventDate, Location location, List<String> amenities, String externalLink, PublicType publicType, int ownerId){
+    this(UUID.randomUUID(), System.currentTimeMillis(), speaker, organization, eventDate, location, amenities, externalLink, publicType, ownerId);
+  }
 
-    public String getOrganization(){
-        return organization;
-    }
+  public Event(UUID id, long timeStamp, String speaker, String organization, Date eventDate, Location location, List<String> amenities, String externalLink, PublicType publicType, int ownerId){
+    this.eventId = id;
+    this.speaker = speaker;
+    this.organization = organization;
+    this.eventDate = eventDate;
+    this.location = location;
+    this.amenities = amenities;
+    this.externalLink = externalLink;
+    this.publicType = publicType;
+    this.ownerId = ownerId;
+    this.thread = new ArrayList<ThreadComment>();
+    this.timeStamp = timeStamp;
+  }
 
-    public Date getEventDate(){
-        return eventDate;
-    }
+  public UUID getEventId(){
+    return eventId;
+  }
 
-    public Location getLocation(){
-        return location;
-    }
+  public String getSpeaker(){
+    return speaker;
+  }
 
-    public List<String> getAmenities(){
-        return amenities;
-    }
+  public String getOrganization(){
+    return organization;
+  }
 
-    public String getExternalLink(){
-        return externalLink;
-    }
+  public Date getEventDate(){
+    return eventDate;
+  }
 
-    public PublicType getPublicType(){
-        return publicType;
-    }
+  public Location getLocation(){
+    return location;
+  }
 
-    public int getOwnerId(){
-        return ownerId;
-    }
+  public List<String> getAmenities(){
+    return amenities;
+  }
 
-    public List<ThreadComment> getThread(){
-        return thread;
-    }
-    
-    public void addThreadComment(ThreadComment comment){
-        thread.add(comment);
-    }
-    
-    public void copyThread(List<ThreadComment> thrd){
-        thread = thrd;
-    }
+  public String getExternalLink(){
+    return externalLink;
+  }
 
-    public long getTimeStamp(){
-        return timeStamp;
-    }
+  public PublicType getPublicType(){
+    return publicType;
+  }
+
+  public int getOwnerId(){
+    return ownerId;
+  }
+
+  public List<ThreadComment> getThread(){
+    return thread;
+  }
+
+  public void addThreadComment(ThreadComment comment){
+    thread.add(comment);
+  }
+
+  public void copyThread(List<ThreadComment> thrd){
+    thread = thrd;
+  }
+
+  public long getTimeStamp(){
+    return timeStamp;
+  }
     
 }
