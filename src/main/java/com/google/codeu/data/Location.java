bcc8a75f2fa1958c37c2ca6468342a9a72f67c25
Pedro de Vasconcellos Oporto <pdevasconcelloso@wpi.edu>
2019-03-28 13:43:17 -0400

    Update Location.java
    Modified indentation to 2sps on Location.java

--- src/main/java/com/google/codeu/data/Location.java
+++ src/main/java/com/google/codeu/data/Location.java
@@ -4,6 +4,59 @@ import java.util.List;
 
 public class Location {
     
+  private double lat;
+  private double lng;
+  private String locationName;
+  private String country;
+  private String state;
+  private String city;
+  private int zipcode;
+  private List<String> adressLines;
+
+  public Location(double lat, double lng, String name, String country, String state, String city, int zipcode, List<String> adressLines){
+    this.lat = lat;
+    this.lng = lng;
+    this.locationName = name;
+    this.country = country;
+    this.state = state;
+    this.city = city;
+    this.zipcode = zipcode;
+    this.adressLines = adressLines;
+  }
+
+  public double getLat() {
+    return lat;
+  }
+
+  public double getLng() {
+    return lng;
+  }
+
+  public String getLocationName(){
+    return locationName;
+  }
+
+  public String getCountry(){
+    return country;
+  }
+
+  public String getState(){
+    return state;
+  }
+
+  public String getCity(){
+    return city;
+  }
+
+  public int getZipcode(){
+    return zipcode;
+  }
+
+  public List<String> getAdressLines(){
+    return adressLines;
+  }
+
+  public static class Builder{
     private double lat;
     private double lng;
     private String locationName;
@@ -12,104 +65,51 @@ public class Location {
     private String city;
     private int zipcode;
     private List<String> adressLines;
-    
-    public Location(double lat, double lng, String name, String country, String state, String city, int zipcode, List<String> adressLines){
-        this.lat = lat;
-        this.lng = lng;
-        this.locationName = name;
-        this.country = country;
-        this.state = state;
-        this.city = city;
-        this.zipcode = zipcode;
-        this.adressLines = adressLines;
-    }
-    
-    public double getLat() {
-        return lat;
-    }
-    
-    public double getLng() {
-        return lng;
-    }
-    
-    public String getLocationName(){
-        return locationName;
-    }
-        
-    public String getCountry(){
-        return country;
+
+    public Builder(){}
+
+    public Builder setLat(double lat){
+      this.lat = lat;
+      return this;
     }
 
-    public String getState(){
-        return state;
+    public Builder setLng(double lng){
+      this.lng = lng;
+      return this;
     }
 
-    public String getCity(){
-        return city;
+    public Builder setLocationName(String locationName){
+      this.locationName = locationName;
+      return this;
     }
 
-    public int getZipcode(){
-        return zipcode;
+    public Builder setCountry(String country){
+      this.country = country;
+      return this;
     }
 
-    public List<String> getAdressLines(){
-        return adressLines;
+    public Builder setState(String state){
+      this.state = state;
+      return this;
     }
-    
-    public static class Builder{
-        private double lat;
-        private double lng;
-        private String locationName;
-        private String country;
-        private String state;
-        private String city;
-        private int zipcode;
-        private List<String> adressLines;
-        
-        public Builder(){}
-        
-        public Builder setLat(double lat){
-            this.lat = lat;
-            return this;
-        }
-        
-        public Builder setLng(double lng){
-            this.lng = lng;
-            return this;
-        }
-        
-        public Builder setLocationName(String locationName){
-            this.locationName = locationName;
-            return this;
-        }
-        
-        public Builder setCountry(String country){
-            this.country = country;
-            return this;
-        }
-        
-        public Builder setState(String state){
-            this.state = state;
-            return this;
-        }
-        
-        public Builder setCity(String city){
-            this.city = city;
-            return this;
-        }
-        
-        public Builder setZipcode(int zipcode){
-            this.zipcode = zipcode;
-            return this;
-        }
-        
-        public Builder setAdressLines(List<String> adressLines){
-            this.adressLines = adressLines;
-            return this;
-        }
-        
-        public Location build(){
-            return new Location(lat, lng, name, country, state, city, zipcode, adressLines)
-        }
-    } 
+
+    public Builder setCity(String city){
+      this.city = city;
+      return this;
+    }
+
+    public Builder setZipcode(int zipcode){
+      this.zipcode = zipcode;
+      return this;
+    }
+
+    public Builder setAdressLines(List<String> adressLines){
+      this.adressLines = adressLines;
+      return this;
+    }
+
+    public Location build(){
+      return new Location(lat, lng, name, country, state, city, zipcode, adressLines)
+    }
+  } 
 }
