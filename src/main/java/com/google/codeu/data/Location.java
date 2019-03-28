9e2599ae5856ce1c3ae3cfda78d20cac5aa57d54
Pedro de Vasconcellos Oporto <pdevasconcelloso@wpi.edu>
2019-03-28 11:22:48 -0400

    Update Location.java
    Removed extra empty lines and implemented a builder so that a location can be created as:
    new Location.Builder()
    .setLat(100)
    .setLng(200)
    ....
    .build();

--- src/main/java/com/google/codeu/data/Location.java
+++ src/main/java/com/google/codeu/data/Location.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Location {
     
-    
     private double lat;
     private double lng;
     private String locationName;
@@ -36,7 +35,6 @@ public class Location {
     public String getLocationName(){
         return locationName;
     }
-
         
     public String getCountry(){
         return country;
@@ -58,4 +56,60 @@ public class Location {
         return adressLines;
     }
     
+    public static class Builder{
+        private double lat;
+        private double lng;
+        private String locationName;
+        private String country;
+        private String state;
+        private String city;
+        private int zipcode;
+        private List<String> adressLines;
+        
+        public Builder(){}
+        
+        public Builder setLat(double lat){
+            this.lat = lat;
+            return this;
+        }
+        
+        public Builder setLng(double lng){
+            this.lng = lng;
+            return this;
+        }
+        
+        public Builder setLocationName(String locationName){
+            this.locationName = locationName;
+            return this;
+        }
+        
+        public Builder setCountry(String country){
+            this.country = country;
+            return this;
+        }
+        
+        public Builder setState(String state){
+            this.state = state;
+            return this;
+        }
+        
+        public Builder setCity(String city){
+            this.city = city;
+            return this;
+        }
+        
+        public Builder setZipcode(int zipcode){
+            this.zipcode = zipcode;
+            return this;
+        }
+        
+        public Builder setAdressLines(List<String> adressLines){
+            this.adressLines = adressLines;
+            return this;
+        }
+        
+        public Location build(){
+            return new Location(lat, lng, name, country, state, city, zipcode, adressLines)
+        }
+    } 
 }
