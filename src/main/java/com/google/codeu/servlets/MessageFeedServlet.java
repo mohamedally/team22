3f9dac0f7b28f69a9ee5d60fd386499f6ac18196
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-04-10 01:03:57 -0400

    added translate function to messagefeedservlet

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -12,6 +12,11 @@ import com.google.codeu.data.Datastore;
 import com.google.codeu.data.Message;
 import com.google.gson.Gson;
 
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.Translate.TranslateOption;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
 /**
  * Handles fetching all messages for the public feed.
  */
@@ -23,6 +28,19 @@ public class MessageFeedServlet extends HttpServlet {
   @Override
   public void init() {
     datastore = new Datastore();
+  }
+
+  private void translateMessages(List<Message> messages, String targetLanguageCode) {
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
+
+    for (Message message : messages) {
+      String originalText = message.getText();
+
+      Translation translation = translate.translate(originalText, TranslateOption.targetLanguage(targetLanguageCode));
+      String translatedText = translation.getTranslatedText();
+
+      message.setText(translatedText);
+    }
   }
 
   /**
